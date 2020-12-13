242f347c0db148d549c1796752900ccf6e2780f4
vincentnerv2020 <73843138+vincentnerv2020@users.noreply.github.com>
2020-12-13 17:55:40 +0300

    Networking first tests

---
+++ MultiplayerProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerMovement : NetworkBehaviour
+{
+  void HandleMovement()
+    {
+        if (isLocalPlayer)
+        {
+            //Do something
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
+            transform.position = transform.position + movement;
+        }
+    }
+
+
+    private void Update()
+    {
+        HandleMovement();
+    }
+}
