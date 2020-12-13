c98beddbb6ec5afc23fa3cc4bfe1ab6b51b08416
vincentnerv2020 <73843138+vincentnerv2020@users.noreply.github.com>
2020-12-13 20:26:02 +0300

    Core Network Fundamentals

--- MultiplayerProject/Assets/Scripts/PlayerMovement.cs
+++ MultiplayerProject/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,10 @@ using Mirror;
 
 public class PlayerMovement : NetworkBehaviour
 {
+
+    [SyncVar(hook = nameof(OnHolaCountChange))]
+    int holaCount = 0;
+
   void HandleMovement()
     {
         if (isLocalPlayer)
@@ -14,6 +18,9 @@ public class PlayerMovement : NetworkBehaviour
             float moveVertical = Input.GetAxis("Vertical");
             Vector3 movement = new Vector3(moveHorizontal * 0.1f, moveVertical * 0.1f, 0);
             transform.position = transform.position + movement;
+
+
+
         }
     }
 
@@ -21,5 +28,43 @@ public class PlayerMovement : NetworkBehaviour
     private void Update()
     {
         HandleMovement();
+        if (isLocalPlayer && Input.GetKeyDown(KeyCode.X))
+        {
+            Debug.Log("Send to a server HOLA!");
+            Hola();
+        }
+        if(isServer && transform.position.y > 50)
+        {
+            TooHigh();
+        }
+    }
+
+
+
+    [Command]
+    void Hola()
+    {
+        Debug.Log("Received HOLA from clien!");
+        holaCount++;
+        RepplyHola(); 
+
+    }
+
+    [TargetRpc]
+    void RepplyHola()
+    {
+        Debug.Log("ReceiveHola");
+    }
+
+    [ClientRpc]
+    void TooHigh()
+    { 
+        Debug.Log("Too High");
+    }
+
+
+    void OnHolaCountChange(int oldCount, int newCount)
+    {
+        Debug.Log($"We had {oldCount} holas, but now we have {newCount} holas");
     }
 }
---
+++ MultiplayerProject_clone_0/Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class MyNetworkManager : NetworkManager
+{
+    //Server
+    public override void OnStartServer()
+    {
+        Debug.Log("Server is Started");
+    }
+
+    public override void OnStopServer()
+    {
+        Debug.Log("Server stopped!");
+    }
+
+    //Client
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log("Client Connected");
+    }
+
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+        Debug.Log("Client disconnected");
+    }
+
+}
